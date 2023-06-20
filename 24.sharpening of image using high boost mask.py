9b713157ff0ae3415c96275036e722410e61833f
arshhh11 <113960199+arshhh11@users.noreply.github.com>
2023-06-20 09:10:36 +0530

    Update 24.sharpening of image using high boost mask.py

--- 24.sharpening of image using high boost mask.py
+++ 24.sharpening of image using high boost mask.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Load the image
-img = cv2.imread("C:/Users/Raju/OneDrive/Documents/computer vision/WhatsApp Image 2022-03-18 at 6.54.12 AM.jpeg")
+img = cv2.imread("C:/Users/arsh/OneDrive/Documents/computer vision/WhatsApp Image 2022-03-18 at 6.54.12 AM.jpeg")
 
 # Convert the image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
