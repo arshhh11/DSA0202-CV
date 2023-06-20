a435848e0d8f53e30fd947c1012c50a5fcb1568b
arshhh11 <113960199+arshhh11@users.noreply.github.com>
2023-06-20 09:15:46 +0530

    Update 33.Morphological operations Morphological Gradient technique..py

--- 33.Morphological operations Morphological Gradient technique..py
+++ 33.Morphological operations Morphological Gradient technique..py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Load the image
-image = cv2.imread('C:/Users/Raja/OneDrive/Documents/computer vision/WhatsApp Image 2022-03-18 at 6.54.12 AM.jpeg', 0)  # Read the image as grayscale
+image = cv2.imread('C:/Users/arsh/OneDrive/Documents/computer vision/WhatsApp Image 2022-03-18 at 6.54.12 AM.jpeg', 0)  # Read the image as grayscale
 
 # Create a kernel for morphological operations
 kernel = np.ones((3, 3), np.uint8)
