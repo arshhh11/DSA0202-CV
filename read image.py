2ea05938b0586369301e382dda4013cbb638d27a
arshhh11 <113960199+arshhh11@users.noreply.github.com>
2023-06-20 09:18:39 +0530

    Update read image.py

--- read image.py
+++ read image.py
@@ -6,7 +6,7 @@ import numpy as np
 kernel = np.ones((5,5),np.uint8)
 print(kernel)
 
-path = "C:/Users/Raja/OneDrive/Pictures/IMG_20180215_224619.jpg"
+path = "C:/Users/arsh/OneDrive/Pictures/IMG_20180215_224619.jpg"
 img =  cv2.imread(path)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
