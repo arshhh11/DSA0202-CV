d67a048d4e0c30ea49419b1eb9e344e2d01a5f31
arshhh11 <113960199+arshhh11@users.noreply.github.com>
2023-06-20 09:17:16 +0530

    Update Smaller Bigger.py

--- Smaller Bigger.py
+++ Smaller Bigger.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread("C:/Users/Raja/OneDrive/Pictures\Saved Pictures/WhatsApp Image 2022-03-18 at 6.54.12 AM.jpeg")
+img = cv2.imread("C:/Users/arsh/OneDrive/Pictures\Saved Pictures/WhatsApp Image 2022-03-18 at 6.54.12 AM.jpeg")
 height, width = img.shape[:2]
 
 scale_factor = 1.5
